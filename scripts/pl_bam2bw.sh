aee66c04f3c08f6d772088cd96431d9999295389
KrisYangPO <kristoff0109@gmail.com>
2024-07-01 20:15:13 +0800

    Adjust overlapped bamcoverage file. divided them into two scripts

---
+++ scripts/pl_bam2bw.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# parameters
+sampleID=$1
+inputpath=$2
+outputpath=$3
+outputformat=$4
+core=$5
+
+# get a ${sampleID}_*.out.bam
+# output name: sampleID_Aligned.sortedByCoord
+cd ${inputpath}
+sample=$(ls ${sampleID}*.out.bam)
+sample=${sample/.out.bam/}
+
+
+# samtool builds index at current folder.
+samtools index \
+ -@ ${core} ${inputpath}/${sample}.out.bam \
+ ${inputpath}/${sample}.out.bam.bai
+
+
+# program
+bamCoverage \
+ --normalizeUsing CPM \
+ --outFileFormat ${outputformat} \
+ --centerReads \
+ --extendReads 150 \
+ -p ${core} \
+ -b ${inputpath}/${sample}.out.bam \
+ -o ${outputpath}/${sample}.${outputformat}
+
